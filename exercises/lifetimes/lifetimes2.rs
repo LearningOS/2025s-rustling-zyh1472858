07d57d768c6251a999919ed1a43fb87b05d29262
zyh1472858 <zyh1472858@gmail.com>
2025-04-10 01:43:09 +0800

    U

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,19 +6,17 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> String{
     if x.len() > y.len() {
-        x
+        x.to_string()
     } else {
-        y
+        y.to_string()
     }
 }
 
 fn main() {
     let string1 = String::from("long string is long");
-    let result;
+    let mut result;
     {
         let string2 = String::from("xyz");
         result = longest(string1.as_str(), string2.as_str());
